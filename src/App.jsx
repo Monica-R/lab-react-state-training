f8578eade18c6b429982540e5ffee2a5d5b84697
Monica-R <moniroka_339@hotmail.com>
2025-02-06 08:00:08 +0000

    work in progress...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import "./App.css";
+import ClickablePicture from "./components/ClickablePicture/ClickablePicture";
+import CounterButton from "./components/CounterButton/CounterButton";
+import Dice from "./components/Dice/Dice";
+import DiscoButton from "./components/DiscoButton/DiscoButton";
+import LikeButton from "./components/LikeButton/LikeButton";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+
+      <main className="main">
+        <h2 className="h2">Like button!</h2>
+        <LikeButton />
+        <hr />
+        <h2 className="h2">Increment number!</h2>
+        <CounterButton />
+        <hr />
+        <h2 className="h2">Change picture</h2>
+        <ClickablePicture />
+        <hr />
+        <h2 className="h2">Dice game</h2>
+        <Dice />
+        <h2 className="h2">Disco Button</h2>
+        <DiscoButton/>
+      </main>
     </div>
   );
 }
